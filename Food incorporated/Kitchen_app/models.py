427757b56a9b8a995e1e04732b4ebd19c1cad0f5
VictorOhason <159486368+VictorOhason@users.noreply.github.com>
2026-02-01 18:22:43 +0000

    Delete Food incorporated directory

--- Food incorporated/Kitchen_app/models.py
+++
@@ -1,7 +0,0 @@
-# models.py
-class Order:
-    def __init__(self, data):
-        self.order_number = data["orderNumber"]
-        self.table = data["tableNumber"]
-        self.items = data["items"]
-        self.status = data.get("status", "pending")
